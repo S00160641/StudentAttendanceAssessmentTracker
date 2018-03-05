6b6d4f81b9eb587ec083399d0160c6447c149119
Sophia Price <sophiaprice10@hotmail.com>
2018-03-05 14:54:27 +0000

    Enable-Migrations, Added Classes

---
+++ RadProject/Models/Lecturer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RadProject.Models
+{
+    [Table("Lecturer")]
+    public class Lecturer
+    {
+        // Lecturer ID
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Lecturer ID")]
+        public int StudentID { get; set; }
+
+        // First Name
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        // Last Name
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        // Modules
+        public virtual Module associatedModules { get; set; }
+    }
+}
---
+++ RadProject/Models/StudentModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RadProject.Models
+{
+    [Table("StudentModule")]
+    public class StudentModule
+    {
+        // Student ID
+        [Display(Name = "Student ID")]
+        [Key, ForeignKey("Student"), Column(Order = 0)]
+        public virtual ICollection<Student> Student { get; set; }
+
+        // Module ID
+        [Display(Name = "Module ID")]
+        [Key, ForeignKey("Module"), Column(Order = 1)]
+        public virtual ICollection<Module> Module { get; set; }
+
+        // Attendance
+        [Display(Name = "Attendance")]
+        public double Attendance { get; set; }
+    }
+}
